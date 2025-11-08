197c760adb24cfd44007182cdf334d41f15d06bb
pankaj-raikar <pankajraikar6@gmail.com>
2025-11-08 13:39:09 +0530

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,37 @@
+import { nextCookies } from 'better-auth/next-js';
+import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { polarClient } from "./lib/payments";
+import prisma from "@nextjs-boilerplate-saas/db";
+
+export const auth = betterAuth<BetterAuthOptions>({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+	plugins: [
+		polar({
+			client: polarClient,
+			createCustomerOnSignUp: true,
+			enableCustomerPortal: true,
+			use: [
+				checkout({
+					products: [
+						{
+							productId: "your-product-id",
+							slug: "pro",
+						},
+					],
+					successUrl: process.env.POLAR_SUCCESS_URL,
+					authenticatedUsersOnly: true,
+				}),
+				portal(),
+			],
+		}),
+    nextCookies()
+  ],
+});
